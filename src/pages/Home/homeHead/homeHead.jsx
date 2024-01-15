6bf4b35cdb0bdcf83dbed3185e7b6d20b584fca7
G1ga6yte <gigabyte10tb@gmail.com>
2024-01-15 20:50:39 +0400

    addingProject

--- src/pages/Home/homeHead/homeHead.jsx
+++ src/pages/Home/homeHead/homeHead.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import "./homeHead.scss"
+import {useInView} from "react-intersection-observer";
 
 function HomeHead (){
+  const {ref: myRef1, inView: visible1} = useInView({
+    "threshold": 0,
+    "triggerOnce": true
+  })
+  const {ref: myRef2, inView: visible2} = useInView({
+    "threshold": 0,
+    "triggerOnce": true
+  })
+  
+  
   return(
      <div className="HomeHead">
-        <p className="header">Digital products</p>
-       <p className="header">creatives & Impressive</p>
+        <p ref={myRef1}  className={`header ${visible1 ? "scrollAnimation" : ""}`}>Digital products</p>
+       <p  ref={myRef1} className={`header ${visible1 ? "scrollAnimation" : ""}`}>creatives & Impressive</p>
 
-       <p className="prg">We use the latest technologies to create innovative and user-friendly solutions that meet the needs of our clients</p>
+       <p ref={myRef2} className={`prg ${visible2 ? "scrollAnimation" : ""}`}>We use the latest technologies to create innovative and user-friendly solutions that meet the needs of our clients</p>
 
      </div>
   )
