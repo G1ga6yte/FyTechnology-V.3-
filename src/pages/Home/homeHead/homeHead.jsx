ed7ac65288cfff19364a4a3601979ec5c207073e
G1ga6yte <gigabyte10tb@gmail.com>
2024-01-31 00:19:52 +0400

    addingProject

--- src/pages/Home/homeHead/homeHead.jsx
+++ src/pages/Home/homeHead/homeHead.jsx
@@ -12,15 +12,24 @@ function HomeHead (){
     "threshold": 0,
     "triggerOnce": true
   })
+  const {ref: myRef3, inView: visible3} = useInView({
+    "threshold": 0,
+    "triggerOnce": true
+  })
   
   
   return(
      <div className="HomeHead">
-        <p ref={myRef1}  className={`header ${visible1 ? "scrollAnimation" : ""}`}>The</p>
-       <p  ref={myRef1} className={`header ${visible1 ? "scrollAnimation" : ""}`}>FyTechnology</p>
-
-       <p ref={myRef2} className={`prg ${visible2 ? "scrollAnimation" : ""}`}>We use the latest technologies to create innovative and user-friendly solutions that meet the needs of our clients</p>
-
+        <div className="headerBlock">
+          <p ref={myRef1}  className={`header ${visible1 ? "scrollAnimation2" : ""}`}>The</p>
+          <p  ref={myRef1} className={`header ${visible1 ? "scrollAnimation" : ""}`}>FyTechnology</p>
+  
+          <p ref={myRef2} className={`prg ${visible2 ? "scrollAnimation" : ""}`}>We use the latest technologies to create innovative and user-friendly solutions that meet the needs of our clients</p>
+          
+        </div>
+       <div className="textBlock">
+         <p ref={myRef3} className={`prgRight ${visible3 ? "scrollAnimation2" : ""}`}>Digital products creatives & Impressive</p>
+       </div>
      </div>
   )
 }
